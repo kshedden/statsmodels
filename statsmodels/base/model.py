475cf416466227eb8319c8f1211f6c3837f2d77e
Kerby Shedden <kshedden@umich.edu>
2014-07-09 12:44:29 -0400

    Intial commit of generic elastic net code

--- statsmodels/base/model.py
+++ statsmodels/base/model.py
@@ -691,6 +691,157 @@ class GenericLikelihoodModel(LikelihoodModel):
         return genericmlefit
     #fit.__doc__ += LikelihoodModel.fit.__doc__
 
+    def fit_regularized(self, method="coord_descent", maxiter=100,
+                        alpha=0., L1_wt=1., start_params=None,
+                        cnvrg_tol=1e-7, zero_tol=1e-8, **kwargs):
+        """
+        Return a regularized fit to a regression model.
+
+        Parameters
+        ----------
+        method :
+            Only the coordinate descent algorithm is implemented.
+        maxiter : integer
+            The maximum number of iteration cycles (an iteration cycle
+            involves running coordinate descent on all variables).
+        alpha : scalar or array-like
+            The penalty weight.  If a scalar, the same penalty weight
+            applies to all variables in the model.  If a vector, it
+            must have the same length as `params`, and contains a
+            penalty weight for each coefficient.
+        L1_wt : scalar
+            The fraction of the penalty given to the L1 penalty term.
+            Must be between 0 and 1 (inclusive).  If 0, the fit is
+            a ridge fit, if 1 it is a lasso fit.
+        start_params : array-like
+            Starting values for `params`.
+        cnvrg_tol : scalar
+            If `params` changes by less than this amount (in sup-norm)
+            in once iteration cycle, the algorithm terminates with
+            convergence.
+        zero_tol : scalar
+            Any estimated coefficient smaller than this value is
+            replaced with zero.
+
+        Returns
+        -------
+        A results object of the same type returned by `fit`.
+
+        Notes
+        -----
+        The penalty is the "elastic net" penalty, which
+        is a convex combination of L1 and L2 penalties.
+
+        The function that is minimized is:
+
+        -loglike/n + alpha*((1-L1_wt)*|params|_2^2/2 + L1_wt*|params|_1)
+
+        where |*|_1 and |*|_2 are the L1 and L2 norms.
+
+        The computational approach used here is to obtain a quadratic
+        approximation to the smooth part of the target function:
+
+        -loglike/n + alpha*(1-L1_wt)*|params|_2^2/2
+
+        then optimize the L1 penalized version of this function along
+        a coordinate axis.
+
+        This is a generic implementation that may be reimplemented in
+        specific models for better performance.
+
+        Post-estimation results are based on the same data used to
+        select variables, hence may be subject to overfitting biases.
+        """
+
+        k_exog = self.exog.shape[1]
+        n_exog = self.exog.shape[0]
+
+        if np.isscalar(alpha):
+            alpha = alpha * np.ones(k_exog, dtype=np.float64)
+
+        # Define starting params
+        if start_params is None:
+            params = np.zeros(k_exog, dtype=np.float64)
+        else:
+            params = start_params.copy()
+
+        # Create a model instance for optimizing a single variable
+        model_1var = copy.deepcopy(self)
+
+        # All the negative penalized loglikeihood functions.
+        def gen_npfuncs(k):
+            def nploglike(params):
+                pen = alpha[k]*((1 - L1_wt)*params**2/2 + L1_wt*np.abs(params))
+                return -model_1var.loglike(np.r_[params]) / n_exog + pen
+            def npscore(params):
+                pen_grad = alpha[k]*(1 - L1_wt)*params
+                return -model_1var.score(np.r_[params])[0] / n_exog + pen_grad
+            def nphess(params):
+                pen_hess = alpha[k]*(1 - L1_wt)
+                return -model_1var.hessian(np.r_[params])[0,0] / n_exog + pen_hess
+            return nploglike, npscore, nphess
+        nploglike_funcs = [gen_npfuncs(k) for k in range(len(params))]
+
+        converged = False
+        btol = 1e-8
+        params_zero = np.zeros(len(params), dtype=bool)
+
+        for itr in range(maxiter):
+
+            # Sweep through the parameters
+            params_save = params.copy()
+            for k in range(k_exog):
+
+                # Under the active set method, if a parameter becomes
+                # zero we don't try to change it again.
+                if params_zero[k]:
+                    continue
+
+                # Set exog to include only the variable whose effect
+                # is being estimated.
+                model_1var.exog = exog[:,k]
+
+                # Set the offset to account for the variables that are
+                # being held fixed.
+                params0 = params.copy()
+                params0[k] = 0
+                v = np.dot(self.surv.exog_s[stx], params0)
+                model_1var.offset = self.offset + v
+
+                params[k] = _opt_1d(nploglike_funcs[k], params[k],
+                                    alpha[k]*L1_wt, tol=btol)
+
+                # Update the active set
+                if itr > 0 and np.abs(params[k]) < zero_tol:
+                    params_zero[k] = True
+                    params[k] = 0.
+
+            # Check for convergence
+            pchange = np.max(np.abs(params - params_save))
+            if pchange < cnvrg_tol:
+                converged = True
+                break
+
+        # Set approximate zero coefficients to be exactly zero
+        params *= np.abs(params) >= zero_tol
+
+        # Fit the reduced model to get standard errors and other
+        # post-estimation results.
+        ii = np.flatnonzero(params)
+        cov = np.zeros((k_exog, k_exog), dtype=np.float64)
+        if len(ii) > 0:
+            model = self.__class__(self.endog, self.exog[:, ii],
+                                   status=self.status, entry=self.entry,
+                                   strata=self.strata, offset=self.offset,
+                                   ties=self.ties, missing=self.missing)
+            rslt = model.fit()
+            cov[np.ix_(ii, ii)] = rslt.normalized_cov_params
+
+        rfit = rslt.__class__(self, params, cov_params=cov)
+        rfit.regularized = True
+
+        return rfit
+
 
 class Results(object):
     """
@@ -2019,5 +2170,8 @@ class GenericLikelihoodModelResults(LikelihoodModelResults, ResultMixin):
                              yname=yname, xname=xname, title=title)
         smry.add_table_params(self, yname=yname, xname=xname, alpha=alpha,
                               use_t=False)
+
+        if hasattr(self, "regularized") and self.regularized == True:
+            smry.add_text("Standard errors do not account for the regularization")
 
         return smry
